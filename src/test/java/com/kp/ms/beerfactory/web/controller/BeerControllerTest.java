1f55b7d38fdccf52a604a64eed8de695ebe0cd7f
bvsspavankumar <bvsspavankumar@gmail.com>
2020-12-16 21:19:39 +0530

    Refactor failing tests. Closes #11

--- src/test/java/com/kp/ms/beerfactory/web/controller/BeerControllerTest.java
+++ src/test/java/com/kp/ms/beerfactory/web/controller/BeerControllerTest.java
@@ -1,5 +1,6 @@
 package com.kp.ms.beerfactory.web.controller;
 
+import java.math.BigDecimal;
 import java.util.UUID;
 
 import org.junit.jupiter.api.Test;
@@ -12,6 +13,7 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.kp.ms.beerfactory.web.models.BeerDto;
+import com.kp.ms.beerfactory.web.models.BeerStyleEnum;
 
 @WebMvcTest(BeerController.class)
 public class BeerControllerTest {
@@ -34,7 +36,7 @@ public class BeerControllerTest {
 	@Test
 	void saveNewBeer() throws Exception {
 		
-		BeerDto b1 = BeerDto.builder().build();
+		BeerDto b1 = getValidBeerDto();
 		String b1toJson = objectMapper.writeValueAsString(b1);
 		
 		mockMvc.perform(MockMvcRequestBuilders
@@ -47,7 +49,7 @@ public class BeerControllerTest {
 	
 	@Test
 	void updateBeerById() throws Exception {
-		BeerDto b2 = BeerDto.builder().build();
+		BeerDto b2 = getValidBeerDto();
 		String b2toJson = objectMapper.writeValueAsString(b2);
 		
 		mockMvc.perform(MockMvcRequestBuilders
@@ -55,5 +57,14 @@ public class BeerControllerTest {
 				.contentType(MediaType.APPLICATION_JSON)
 				.content(b2toJson))
 				.andExpect(MockMvcResultMatchers.status().isNoContent());
+	}
+	
+	BeerDto getValidBeerDto() {
+		return BeerDto.builder()
+				.beerName("My Beer")
+				.beerStyle(BeerStyleEnum.ALE)
+				.price(new BigDecimal("2.33"))
+				.upc(432112344321L)
+				.build();
 	}
 }
