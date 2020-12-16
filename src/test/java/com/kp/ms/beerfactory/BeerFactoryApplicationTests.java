9549255d4c333dfe2b2f6c97b2c3e222529f3cb9
bvsspavankumar <bvsspavankumar@gmail.com>
2020-12-16 17:01:27 +0530

    Adding initial project. Closes #1

---
+++ src/test/java/com/kp/ms/beerfactory/BeerFactoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kp.ms.beerfactory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerFactoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
